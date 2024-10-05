1d3d1c3189c6d334d01d97a4b208d0439157b222
Shafinam4041 <115705683+Shafinam4041@users.noreply.github.com>
2024-10-06 00:36:47 +0530

    Add files via upload

---
+++ Prodigy-1/script.js
@@ -0,0 +1,10 @@
+// Change header background color on scroll
+window.addEventListener('scroll', function() {
+    const header = document.getElementById('header');
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
+
